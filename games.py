664835abd7ce7b19ea854969353e13138e740b27
Daphne LeSueur <daphnelesueur@gmail.com>
2023-11-29 20:47:47 -0700

    add selectbox

--- games.py
+++ games.py
@@ -5,3 +5,8 @@ import plotly.express as px
 
 st.title('2022-23 NBA Season')
 
+st.text('Distribution of Points by Team')
+
+df = pd.read_csv('pointsPerGame.csv')
+team = st.selectbox('Select a team', 'df['full name']')
+
