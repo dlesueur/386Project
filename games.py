19e506d0da00aba67edf5265245c478553888d51
Daphne LeSueur <daphnelesueur@gmail.com>
2023-11-30 13:05:54 -0700

    change axis titles

--- games.py
+++ games.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 st.title('2022-23 NBA Season')
 
-st.text('Distribution of Points by Team When Home and Visitng')
+st.text('Distribution Of Points When At Home')
 
 df = pd.read_csv('games.csv')
 teams = pd.read_csv('teams')
@@ -13,7 +13,9 @@ teams_list = teams['full_name']
 team = st.selectbox('Select a team', teams_list)
 
 team_df = df[df['home_team_name'] == team]
-fig = px.histogram(team_df, x='home_team_score', nbins=20, title='Home Team Score Distribution')
+fig = px.histogram(team_df, x='home_team_score', nbins=20, title='Score Distribution When Playing At Home', color = 'Red')
+fig.update_xaxes('Score')
+fig.update_yaxes('Frequency')
 st.plotly_chart(fig)
 
 
