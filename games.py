d10f3fe65d7830055e21ffc29b23186cc7301db6
Daphne LeSueur <daphnelesueur@gmail.com>
2023-11-30 12:59:23 -0700

    add plot to streamlit and other changes

--- games.py
+++ games.py
@@ -5,9 +5,14 @@ import plotly.express as px
 
 st.title('2022-23 NBA Season')
 
-st.text('Distribution of Points by Team')
+st.text('Distribution of Points by Team When Home and Visitng')
 
-df = pd.read_csv('pointsPerGame.csv')
-team = st.selectbox('Select a team', df['full_name'])
+df = pd.read_csv('games.csv')
+teams = pd.read_csv('teams')
+team = st.selectbox('Select a team', teams['full_name'])
+
+team_df = df[df['home_team_name'] == team]
+fig = px.histogram(team_df, x='home_team_score', nbins=20, title='Home Team Score Distribution')
+st.plotly_chart(fig)
 
 
