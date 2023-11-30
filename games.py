c33b2123dfdf6e73bf10ffc05c2a31895a0c3a73
Daphne LeSueur <daphnelesueur@gmail.com>
2023-11-30 13:00:51 -0700

    fix teams_list

--- games.py
+++ games.py
@@ -9,7 +9,8 @@ st.text('Distribution of Points by Team When Home and Visitng')
 
 df = pd.read_csv('games.csv')
 teams = pd.read_csv('teams')
-team = st.selectbox('Select a team', teams['full_name'])
+teams_list = teams['full_name']
+team = st.selectbox('Select a team', teams_list)
 
 team_df = df[df['home_team_name'] == team]
 fig = px.histogram(team_df, x='home_team_score', nbins=20, title='Home Team Score Distribution')
