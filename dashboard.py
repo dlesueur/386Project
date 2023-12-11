40d59ea871f9af43dab226297e4a8257efa51d9f
Daphne LeSueur <daphnelesueur@gmail.com>
2023-12-11 09:40:50 -0700

    add team colors

--- dashboard.py
+++ dashboard.py
@@ -11,12 +11,14 @@ teams = pd.read_csv('datasets/teams.csv')
 teams_list = teams['full_name']
 team = st.selectbox('Select a team', teams_list, key = "selectbox1")
 
+
 df = pd.read_csv('datasets/games.csv')
+colors = pd.read_csv('datasets/teamColors.csv')
 team_df = df[df['home_team_name'] == team]
-fig = px.histogram(team_df, x='home_team_score', nbins=20)
+fig = px.histogram(team_df, x='home_team_score', nbins=20, color = 'team', color_discrete_map=colors)
 fig.update_xaxes(title_text = 'Score')
 fig.update_yaxes(title_text = 'Frequency')
-st.plotly_chart(fig)
+st.plotly_chart(fig, theme = None)
 
 
 st.text('Season Record Against Other Teams')
