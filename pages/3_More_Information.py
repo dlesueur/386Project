95b871c0ef3a4245730c2abbf2e26fb62d7615ca
Daphne LeSueur <daphnelesueur@gmail.com>
2023-12-17 15:19:01 -0700

    change more information

--- pages/3_More_Information.py
+++ pages/3_More_Information.py
@@ -7,9 +7,9 @@ import matplotlib as plt
 
 
 st.title("More Information")
-st.text("This data was taken from the [Ball Don't Lie API](https://www.balldontlie.io/home.html#introduction).")
-st.text("Team color information was taken from [Team Color Codes](https://teamcolorcodes.com/nba-team-color-codes/)")
-st.text("For a demonstration of data collection, view my [blog post](https://dlesueur.github.io/Ball-Don't-Lie/) about the process I used to access this API.")
-st.text("For more analysis of these graphs and a more thorough EDA, view this [blog post](https://dlesueur.github.io/EDA-Don't-Lie/)")
-st.text("Here is my [repository](https://github.com/dlesueur/386Project) containing all of the code for this dashboard, as well as all of the code used to collet, clean, and explore this data.")
+st.write("This data was taken from the [Ball Don't Lie API](https://www.balldontlie.io/home.html#introduction).")
+st.write("Team color information was taken from [Team Color Codes](https://teamcolorcodes.com/nba-team-color-codes/)")
+st.write("For a demonstration of data collection, view my [blog post](https://dlesueur.github.io/Ball-Don't-Lie/) about the process I used to access this API.")
+st.write("For more analysis of these graphs and a more thorough EDA, view this [blog post](https://dlesueur.github.io/EDA-Don't-Lie/)")
+st.write("Here is my [repository](https://github.com/dlesueur/386Project) containing all of the code for this dashboard, as well as all of the code used to collet, clean, and explore this data.")
 
